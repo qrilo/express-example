f74be9b5db9f14abb69ff97da7582b909701c255
Alexei <alexei.solo@outlook.com>
2024-05-25 21:42:37 +0300

    add cookie

--- src/app.js
+++ src/app.js
@@ -10,14 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
-
-app.use(cookieParser());
-
 app.use(express.json());
-
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: true }));
 app.use("/api", routes);
-
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
--- src/routes/users.js
+++ src/routes/users.js
@@ -16,7 +16,16 @@ router.post(
   asyncHandler(async (req, res) => {
     const response = await login(req.body);
 
-    res.cookie("token", response.token, { httpOnly: true, secure: true });
+    const expirationDate = new Date();
+    expirationDate.setHours(expirationDate.getHours() + 1);
+
+    res.cookie("token", response.token, {
+      httpOnly: false,
+      sameSite: "none",
+      secure: true,
+      expires: expirationDate,
+    });
+
     return res
       .status(200)
       .json({ id: response.id, username: response.username });
@@ -36,7 +45,9 @@ router.post(
 router.post(
   "/logout",
   asyncHandler(async (req, res) => {
-    res.clearCookie("auth");
+    res.clearCookie("token");
+
+    return res.status(200).json();
   })
 );
 
